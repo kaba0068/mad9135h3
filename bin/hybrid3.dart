7f6f993a06503895573276bd89682cabf1484a4a
Hugues <hugues@MacBook-Pro-de-Hugues.local>
2024-10-13 15:34:25 -0400

    Setting up initial requirements

--- bin/hybrid3.dart
+++ bin/hybrid3.dart
@@ -1,5 +1,42 @@
 import 'package:hybrid3/hybrid3.dart' as hybrid3;
+import "dart:convert";
+
+class Student {
+  Student(List<Map<String, String>> people) {
+    print("object");
+  }
+
+  void sort(String field) {
+    //todo Sort the items in the list based on "field"
+  }
+
+  void output() {
+    //todo Loop through list and print every item
+  }
+
+  void plus(Map<String, String> person) {
+    //todo Add a new person to the list
+  }
+
+  void remove(String field) {
+    //todo Remove the first item in the list that matches "field" using removeWhere()
+  }
+}
 
 void main(List<String> arguments) {
-  print('Hello world: ${hybrid3.calculate()}!');
+  String studentJSON = '''
+[
+  {"first":"Steve", "last":"Griffith", "email":"griffis@algonquincollege.com"},
+  {"first":"Adesh", "last":"Shah", "email":"shaha@algonquincollege.com"},
+  {"first":"Tony", "last":"Davidson", "email":"davidst@algonquincollege.com"},
+  {"first":"Adam", "last":"Robillard", "email":"robilla@algonquincollege.com"}
+]
+''';
+
+  List studentLIST = json.decode(studentJSON);
+
+  List<Map<String, String>> studentMAP =
+      studentLIST.map((item) => Map<String, String>.from(item)).toList();
+
+  print(studentMAP[0] is Map<String, String>);//checking to see if the type is right
 }
